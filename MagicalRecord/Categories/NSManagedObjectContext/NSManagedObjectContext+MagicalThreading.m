8bc57e180d248d0f31e3a215f78ab3fddd4edac9
jerry <jerry@mobiata.com>
2013-01-20 21:11:04 -0500

    HAW-specific hack to context-confine all save operations.

--- MagicalRecord/Categories/NSManagedObjectContext/NSManagedObjectContext+MagicalThreading.m
+++ MagicalRecord/Categories/NSManagedObjectContext/NSManagedObjectContext+MagicalThreading.m
@@ -6,11 +6,13 @@
 //  Copyright (c) 2012 Magical Panda Software LLC. All rights reserved.
 //
 
+#import <objc/runtime.h>
 #import "NSManagedObjectContext+MagicalThreading.h"
 #import "NSManagedObject+MagicalRecord.h"
 #import "NSManagedObjectContext+MagicalRecord.h"
 
 static NSString const * kMagicalRecordManagedObjectContextKey = @"MagicalRecord_NSManagedObjectContextForThreadKey";
+static char *cdQueueKey;
 
 @implementation NSManagedObjectContext (MagicalThreading)
 
@@ -27,6 +29,12 @@ static NSString const * kMagicalRecordManagedObjectContextKey = @"MagicalRecord_
 	}
 	else
 	{
+        const char *qName = dispatch_queue_get_label(dispatch_get_current_queue());
+        NSString *queueName = [NSString stringWithCString:qName encoding:NSUTF8StringEncoding];
+        if ([queueName isEqualToString:@"CoreData"]) {
+            return [NSManagedObjectContext HAW_contextForCoreDataQueue];
+        }
+        
 		NSMutableDictionary *threadDict = [[NSThread currentThread] threadDictionary];
 		NSManagedObjectContext *threadContext = [threadDict objectForKey:kMagicalRecordManagedObjectContextKey];
 		if (threadContext == nil)
@@ -36,6 +44,15 @@ static NSString const * kMagicalRecordManagedObjectContextKey = @"MagicalRecord_
 		}
 		return threadContext;
 	}
+}
+
++ (NSManagedObjectContext *) HAW_contextForCoreDataQueue {
+    NSManagedObjectContext *ctx = objc_getAssociatedObject(self, &cdQueueKey);
+    if (ctx == nil) {
+        ctx = [self MR_contextWithParent:[NSManagedObjectContext MR_defaultContext]];
+        objc_setAssociatedObject(self, &cdQueueKey, ctx, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    return ctx;
 }
 
 @end
